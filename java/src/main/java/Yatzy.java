c4cdc6420f48bf4ed085f9a8c6451323c86e60af
AbdelilahRami <ramiabdelilah1@gmail.com>
2024-02-12 15:09:56 +0100

    ones and twos methods

--- java/src/main/java/Yatzy.java
+++ java/src/main/java/Yatzy.java
@@ -24,25 +24,13 @@ public class Yatzy {
     }
 
     public static int ones(int d1, int d2, int d3, int d4, int d5) {
-        int sum = 0;
-        if (d1 == 1) sum++;
-        if (d2 == 1) sum++;
-        if (d3 == 1) sum++;
-        if (d4 == 1) sum++;
-        if (d5 == 1) 
-            sum++;
-
-        return sum;
+        return (int) Stream.of(d1, d2, d3, d4, d5)
+            .filter(integer -> integer.equals(1)).count() ;
     }
 
     public static int twos(int d1, int d2, int d3, int d4, int d5) {
-        int sum = 0;
-        if (d1 == 2) sum += 2;
-        if (d2 == 2) sum += 2;
-        if (d3 == 2) sum += 2;
-        if (d4 == 2) sum += 2;
-        if (d5 == 2) sum += 2;
-        return sum;
+        return (int) Stream.of(d1, d2, d3, d4, d5)
+            .filter(integer -> integer.equals(2)).count() * 2;
     }
 
     public static int threes(int d1, int d2, int d3, int d4, int d5) {
